422cca6ce5e251724bccb646c214e0bf061360ab
Hamsa Awad <hamsa.awad.123567@gmail.com>
2023-12-12 14:13:29 +0200

    k.ui;yi

--- app/src/main/java/Afifi/Majd/Majd/majdafefe12/data/AppDatabase.java
+++ app/src/main/java/Afifi/Majd/Majd/majdafefe12/data/AppDatabase.java
@@ -1,4 +1,19 @@
 package Afifi.Majd.Majd.majdafefe12.data;
 
-public class AppDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import Afifi.Majd.Majd.majdafefe12.data.MyTasksTable.MyTask;
+import Afifi.Majd.Majd.majdafefe12.data.MyTasksTable.MyTaskQuery;
+import Afifi.Majd.Majd.majdafefe12.data.SubjectTable.MySubject;
+import Afifi.Majd.Majd.majdafefe12.data.usersTable.MyUser;
+import Afifi.Majd.Majd.majdafefe12.data.usersTable.MyUserQuery;
+
+@Database(entities = {MyUser.class, MySubject.class, MyTask.class},version = 1)
+public abstract class AppDatabase extends RoomDatabase
+{
+    private static AppDatabase db;
+    public abstract MyUserQuery getMyUserQuery();
+    public abstract MyTaskQuery getMyTaskQuery();
+
 }
--- app/src/main/java/Afifi/Majd/Majd/majdafefe12/data/SubjectTable/MySubjectQuery.java
+++ app/src/main/java/Afifi/Majd/Majd/majdafefe12/data/SubjectTable/MySubjectQuery.java
@@ -1,7 +1,23 @@
 package Afifi.Majd.Majd.majdafefe12.data.SubjectTable;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface MySubjectQuery {
+    @Query("SELECT * FROM MySubject")
+    List<MySubject>getAllSubjects();
+    @Insert
+    void insert(MySubject...s);
+    void update(MySubject...s);
+    @Delete
+    void deleteTask(MySubject...s);
+    @Query("DELETE FROM MySubject WHERE Key_id=:keyid")
+    void delete(long keyid);
+    @Query("SELECT * FROM MySubject WHERE title=:sub")
+    MySubject checkSubject(String sub);
 }
