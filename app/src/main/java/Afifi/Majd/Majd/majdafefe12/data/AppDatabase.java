85c85fa61049c8ed866fa2a1a3ac94bf16893efa
Hamsa Awad <hamsa.awad.123567@gmail.com>
2023-12-17 10:33:59 +0200

    k.ui;yi

--- app/src/main/java/Afifi/Majd/Majd/majdafefe12/data/AppDatabase.java
+++ app/src/main/java/Afifi/Majd/Majd/majdafefe12/data/AppDatabase.java
@@ -1,19 +1,39 @@
 package Afifi.Majd.Majd.majdafefe12.data;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import Afifi.Majd.Majd.majdafefe12.data.MyTasksTable.MyTask;
 import Afifi.Majd.Majd.majdafefe12.data.MyTasksTable.MyTaskQuery;
 import Afifi.Majd.Majd.majdafefe12.data.SubjectTable.MySubject;
+import Afifi.Majd.Majd.majdafefe12.data.SubjectTable.MySubjectQuery;
 import Afifi.Majd.Majd.majdafefe12.data.usersTable.MyUser;
 import Afifi.Majd.Majd.majdafefe12.data.usersTable.MyUserQuery;
 
 @Database(entities = {MyUser.class, MySubject.class, MyTask.class},version = 1)
-public abstract class AppDatabase extends RoomDatabase
-{
+public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase db;
+
     public abstract MyUserQuery getMyUserQuery();
+
+    public abstract MySubjectQuery getMySubjectQuery();
+
     public abstract MyTaskQuery getMyTaskQuery();
 
+    public static AppDatabase getDb(Context context)
+    {
+    if(db==null)
+
+    {
+
+        db = Room.databaseBuilder(context, AppDatabase.class, "samihDataBase")
+                .fallbackToDestructiveMigration()
+                .allowMainThreadQueries()
+                .build();
+    }
+    return db;
+ }
 }
