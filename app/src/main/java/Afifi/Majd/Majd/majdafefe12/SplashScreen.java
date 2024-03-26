4d276d95573f2a69cfb055f0ffaa030190396251
hallashuli <hallaradwanshuli@gmail.com>
2024-03-26 12:53:27 +0200

    bvhjfvcgh

---
+++ app/src/main/java/Afifi/Majd/Majd/majdafefe12/SplashScreen.java
@@ -0,0 +1,17 @@
+package Afifi.Majd.Majd.majdafefe12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+     //'lk;lkl;k;lk;l
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
